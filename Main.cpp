42b9f0288b9407d1a46169113d8a7e57262c0152
Constructer-SegmentTree <HelloMinecraft2024@163.com>
2024-11-09 16:26:23 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,9 @@ Server server;
 int main(void) {
     server.InitWinSock();
     server.CreateSocket();
-    server.BindSocket(8080);
+    server.BindSocket(8080,"0.0.0.0");
+    server.ListenSocket(3);
+    SOCKET Sock = server.AcceptSocket();
     server.ExitWinSock();
     system("pause");
     return 0;
