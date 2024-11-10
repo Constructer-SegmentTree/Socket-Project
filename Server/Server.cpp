917b71eeb4c1ab3ae63983b28886aa9791955a5d
Constructer-SegmentTree <HelloMinecraft2024@163.com>
2024-11-10 17:26:09 +0800

    Update Server.cpp

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -7,6 +7,8 @@ int main(void) {
     server.BindSocket(8080,"0.0.0.0");
     server.ListenSocket(3);
     SOCKET Sock = server.AcceptSocket();
+    server.SendData(Sock,"Hello world!");
+    cout << server.RecvData(Sock) << endl;
     server.ExitWinSock();
     system("pause");
     return 0;
